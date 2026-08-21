a57462d50825e835e449066cd001015e9c96e959
Krishna Kumar <kkbhagat20@gmail.com>
2026-08-21 15:36:22 +0530

    feat: configure frontend API services

---
+++ src/service/config.tsx
@@ -0,0 +1,11 @@
+// import { Platform } from "react-native";
+
+// export const BASE_URL =
+//   Platform.OS === "android" ? "http://10.0.2.2:3000" : "http://localhost:3000";
+// export const SOCKET_URL =
+//   Platform.OS === "android" ? "ws://10.0.2.2:3000" : "ws://localhost:3000";
+
+export const BASE_URL = "https://rnpocketplay.onrender.com";
+export const SOCKET_URL = "https://rnpocketplay.onrender.com";
+
+export const VIDEO_URL = "https://rnpocketplay.onrender.com/hls/output.m3u8";
